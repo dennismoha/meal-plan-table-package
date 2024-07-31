1c36d3f8ea9278e97b00a57fa57d3d79a02b9b76
dennismoha <dennismoha2@gmail.com>
2024-07-31 10:01:48 +0300

    added rollup config

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+const packageJson = require('./package.json');
+
+export default [
+  {
+    input: 'src/main.tsx',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourceMap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourceMap: true,
+      },
+    ],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.app.json' })],
+  },
+  {
+    input: 'dist/esm/types/main.d.ts',
+    output: [{ file: 'dist/main.d.ts', format: 'esm' }],
+    plugins: [dts.default()],
+  },
+];
